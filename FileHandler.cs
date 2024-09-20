066528bcbcd3883b742c8f7fe956367a2e735d76
Nyasper <herreragonzalo39@gmail.com>
2024-09-20 03:45:37 -0300

    generateJSONandHTML

--- FileHandler.cs
+++ FileHandler.cs
@@ -54,9 +54,59 @@ public static class FileHandler
     IGrouping<string, Student>[] allSchools = students.GroupBy((s)=>s.school).ToArray();
 
     // Generar el contenido HTML
-    const string HTMLHeader = "<html>\n<head>\n<link rel=\"stylesheet\" href=\"styles.css\">\n<title>Students Images Preview</title>\n</head>\n<body>";
-    const string HTMLFooter = "\n</body>\n</html>";
+    const string HTMLHeader = "<html>\n<head>\n<link rel=\"stylesheet\" href=\"style.css\">\n<title>Students Images Preview</title>\n</head>\n<body>";
+    const string stylesCSS = @"
+:root {
+    color-scheme: light dark;
+}
 
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    padding: 0 15px;
+    background-color: rgb(26, 26, 26);
+}
+
+.schoolTitle {
+    font-size: 2.6;
+    margin: 15px 0;
+    text-align: center;
+}
+
+.schoolContainer {
+    border-radius: 10px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    place-items: center;
+    margin: 15px 0;
+    padding: 10px 20px;
+    background-color: rgba(53, 53, 53, 0.473);
+}
+
+.studentContainer > h2 {
+    text-align: center;
+    margin: 5px 0;
+}
+
+.imageContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 160px;
+    width: 300px;
+    margin: 5px 0;
+}
+
+.imageContainer img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+    overflow: hidden;
+}";
+    const string HTMLFooter = $"\n<style>{stylesCSS}\n</style>\n</body>\n</html>";
     List<string> SchoolContainer = allSchools.Select(school=> string.Join("\n",[$"\n<h2 class=\"schoolTitle\">{school.Key}</h2>\n  <div class=\"schoolContainer\">\n ",string.Join("\n",school.Select((student, index) => 
       $" <div class=\"studentContainer\">\n  <h2>{index + 1}: {student.charaName}</h2>\n  <div class=\"imageContainer\">\n   <img src=\"media/{student.school}/{student.charaName}.png\" class=\"profileImage\" alt=\"profileImage of {student.charaName}\"></img>\n   <img src=\"media/{student.school}/{student.charaName}_full.png\" class=\"fullImage\" alt=\"fullImage of {student.charaName}\">\n </div>\n</div>")),"</div>"])).ToList();
 
